4fe6a9b245c97a60451d5d733d245b454e701c4c
wng <sunwgna@gmail.com>
2022-03-23 21:05:23 +0900

    in progress

--- ppo.py
+++ ppo.py
@@ -10,6 +10,8 @@ import numpy as np
 
 class PPO(nn.Module):
     def __init__(self, dim_in, dim_out):
+        # split dim_in dim_out
+        self.data = []
         self.fc1 = nn.Linear(dim_in, 256)
         self.fc2 = nn.Linear(dim_in, dim_out)
         self.relu = nn.ReLU()
@@ -17,14 +19,29 @@ class PPO(nn.Module):
     def pi(self, x):
         x = self.fc1(x)
         x = self.relu(x)
-        action = self.fc2(x) # 6D output (3 positions + 3 positions)
+        action = self.fc2(x) # 3D output (position of eef)
+        # add normalization
         return action
+    
+    def value(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        value_hat = self.fc2(x)
+        return value_hat
 
     def get_data(self, data):
-        pass
+        self.data.append(data)
 
     def make_batch(self):
-        pass
+        s_list, a_list, r_list = [], [], []
+        for transition in self.data:
+            s_i, a_i, r_i = transition
+            s_list.append(s_i)
+            a_list.append(a_i)
+            r_list.append(r_i)
+        
+        s, a, r = torch.tensor(s_list, dtype=torch.float), torch.tensor(a_list, dtype=torch.float), torch.tensor(r_list, torch.float)
+        return s, a, r
 
     def train(self):
-        pass
+        s, a, r = self.make_batch()
