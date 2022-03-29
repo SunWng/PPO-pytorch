b720d4cf9b1e53af722daec90b0217c3cc1e50d6
wng <sunwgna@gmail.com>
2022-03-29 20:34:50 +0900

    in progress

--- ppo.py
+++ ppo.py
@@ -9,24 +9,25 @@ import torch.optim as optim
 import numpy as np
 
 class PPO(nn.Module):
-    def __init__(self, dim_in, dim_out):
+    def __init__(self, dim_in, pi_out, value_out=1):
         # split dim_in dim_out
         self.data = []
         self.fc1 = nn.Linear(dim_in, 256)
-        self.fc2 = nn.Linear(dim_in, dim_out)
+        self.fc_pi = nn.Linear(dim_in, pi_out)
+        self.fc_value = nn.Linear(dim_in, value_out)
         self.relu = nn.ReLU()
 
     def pi(self, x):
         x = self.fc1(x)
         x = self.relu(x)
-        action = self.fc2(x) # 3D output (position of eef)
+        action = self.fc_pi(x) # 3D output (position of eef)
         # add normalization
         return action
     
     def value(self, x):
         x = self.fc1(x)
         x = self.relu(x)
-        value_hat = self.fc2(x)
+        value_hat = self.fc_value(x)
         return value_hat
 
     def get_data(self, data):
@@ -45,3 +46,13 @@ class PPO(nn.Module):
 
     def train(self):
         s, a, r = self.make_batch()
+        
+        advantage_list = []
+        advantage = 0
+        
+        for s_i, a_i, r_i in s, a, r:
+            advantage = r_i - self.value(s_i)
+            advantage_list.append(advantage)
+        
+        advantage = torch.tensor(advantage, dtype=torch.float)
+        
