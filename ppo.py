5b5dd69bc129db24bfee48b9f4139e0792e4f1a4
SunWng <sunwgna@gmail.com>
2022-04-11 09:37:52 +0900

    in progress

--- ppo.py
+++ ppo.py
@@ -5,29 +5,60 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.distributions import Normal
 
 import numpy as np
 
+## Hyperparatemers ##
+k_epoch = 10
+learning_rate = 0.001
+eps_clip = 0.2
+
 class PPO(nn.Module):
-    def __init__(self, dim_in, pi_out, value_out=1):
+    def __init__(self):
         # split dim_in dim_out
         self.data = []
-        self.fc1 = nn.Linear(dim_in, 256)
-        self.fc_pi = nn.Linear(dim_in, pi_out)
-        self.fc_value = nn.Linear(dim_in, value_out)
+        self.fc1 = nn.Linear(32*64*64, 16384)
+        self.fc2 = nn.Linear(16384, 4096)
+        self.fc3 = nn.Linear(4096, 1024)
+        self.fc4 = nn.Linear(1024, 256)
         self.relu = nn.ReLU()
+        self.pi_mu = nn.Linear(256, 1)
+        self.pi_std = nn.Linear(256, 1)
+        self.v_hat = nn.Linear(256, 1)
+        
+        # input = 3 x 256 x 256
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 16, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(16, 32, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten() # output = 32 * 64 * 64
+        )
+        
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
 
     def pi(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        action = self.fc_pi(x) # 3D output (position of eef)
-        # add normalization
-        return action
+        x = self.conv(x)
+        x = self.relu(self.fc1)
+        x = self.relu(self.fc2)
+        x = self.relu(self.fc3)
+        x = self.relu(self.fc4)
+        pi_mu = torch.tanh(self.pi_mu)
+        pi_std = F.softplus(self.pi_std)
+        
+        return pi_mu, pi_std
     
     def value(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        value_hat = self.fc_value(x)
+        x = self.conv(x)
+        x = self.relu(self.fc1)
+        x = self.relu(self.fc2)
+        x = self.relu(self.fc3)
+        x = self.relu(self.fc4)
+        value_hat = self.v_hat(x)
+        
         return value_hat
 
     def get_data(self, data):
@@ -47,6 +78,8 @@ class PPO(nn.Module):
     def train(self):
         s, a, r = self.make_batch()
         
+        data = self.make_batch()
+        
         advantage_list = []
         advantage = 0
         
@@ -56,3 +89,20 @@ class PPO(nn.Module):
         
         advantage = torch.tensor(advantage, dtype=torch.float)
         
+        # ratio calculation
+        
+        for i in k_epoch:
+            for mini_batch in data:
+                s, a, r, adv = mini_batch
+                
+                mu, std = self.pi(s)
+                dist = Normal(mu, std)
+                log_prob = dist.log_prob(a)
+                ratio = torch.exp(log_prob - old_log_prob)
+                obj_1 = ratio * adv
+                obj_2 = torch.clamp(ratio, 1-eps_clip, 1+eps_clip) * adv
+                loss = -torch.min(obj_1, obj_2)
+                
+                self.optimizer.zero_grad()
+                loss.mean().backward()
+                self.optimizer.step()
